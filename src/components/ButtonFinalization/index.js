10a4b33b685aefc3f2de34195f92bcfc2ecd2056
Sara Rocha de Oliveira <sararocha878@gmail.com>
2023-02-28 22:52:19 -0300

    feat: button finalization and button selected

---
+++ src/components/ButtonFinalization/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ContainerButton } from './styles';
+
+function ButtonFinalization({ children }) {
+  return (
+    <ContainerButton >
+      {children}
+    </ContainerButton>
+  );
+}
+
+export default ButtonFinalization;
