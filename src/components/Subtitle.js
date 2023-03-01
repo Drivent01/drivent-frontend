1218e31c8f376a4a55cfad7463e6ef4039ba1cfe
Cintra <gui7lk@gmail.com>
2023-02-28 23:41:09 -0300

    feat: payment layout

---
+++ src/components/Subtitle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.h4`
+  font-family: sans-serif !important;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  color: #8E8E8E;
+`;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,39 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import Subtitle from '../../../components/Subtitle';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant='h4'>Ingresso e Pagamento</StyledTypography>
+      <StyledSubtitle>Primeiro, escolha sua modalidade de ingresso</StyledSubtitle>
+      <Container>
+        <Box />
+        <Box />
+      </Container>
+      <StyledSubtitle>Fechado! O total ficou em R$ 100. Agora é só confirmar:</StyledSubtitle>
+      <button>RESERVAR INGRESSO</button>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const StyledSubtitle = styled(Subtitle)`
+  margin-bottom: 17px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+  margin-bottom: 44px;
+`;
+
+const Box = styled.div`
+  width: 145px;
+  height: 145px;
+  border: 1px solid #CECECE;
+  border-radius: 20px;
+`;
