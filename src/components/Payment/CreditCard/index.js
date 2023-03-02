137a0223588d271f00e8d318093a332be67b1eba
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-02 15:54:30 -0300

    feat: credit card basic styles

--- src/components/Payment/CreditCard/index.js
+++ src/components/Payment/CreditCard/index.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
-import React from 'react';
+import { Container, CardCredentials } from './styles';
+import ButtonFinalization from '../ButtonFinalization';
 
 export default function PaymentForm(props) {
   const [state, setState] = useState({
@@ -23,82 +24,31 @@ export default function PaymentForm(props) {
   };
 
   return (
-    <div id="PaymentForm">
-      <Cards cvc={state.cvc} expiry={state.expiry} focused={state.focus} name={state.name} number={state.number} />
-      <form>
-        <input
-          type="tel"
-          name="number"
-          placeholder="Card Number"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Card Holder Name"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input type="tel" name="cvc" placeholder="CVC" onChange={handleInputChange} onFocus={handleInputFocus} />
-        <input
-          type="tel"
-          name="expiry"
-          placeholder="Expiration Date"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-      </form>
-    </div>
+    <Container>
+      <div id="PaymentForm">
+        <Cards cvc={state.cvc} expiry={state.expiry} focused={state.focus} name={state.name} number={state.number} />
+        <CardCredentials>
+          <input
+            type="tel"
+            name="number"
+            placeholder="Card Number"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <input type="text" name="name" placeholder="Name" onChange={handleInputChange} onFocus={handleInputFocus} />
+          <input type="tel" name="cvc" placeholder="CVC" onChange={handleInputChange} onFocus={handleInputFocus} />
+          <input
+            type="tel"
+            name="expiry"
+            placeholder="Valid Thru"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+        </CardCredentials>
+      </div>
+      <ButtonFinalization>
+        <p className="title"> FINALIZAR PAGAMENTO</p>
+      </ButtonFinalization>
+    </Container>
   );
 }
-
-// export default function PaymentForm(props) {
-//   const [state, setState] = useState({
-//     cvc: '',
-//     expiry: '',
-//     focus: '',
-//     name: '',
-//     number: '',
-//   });
-
-//   const handleInputFocus = (e) => {
-//     setState({ focus: e.target.name });
-//   };
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-
-//     setState({ [name]: value });
-//   };
-
-//   return (
-//     <div id="PaymentForm">
-//       <Cards cvc={state.cvc} expiry={state.expiry} focused={state.focus} name={state.name} number={state.number} />
-//       <form>
-//         <input
-//           type="tel"
-//           name="number"
-//           placeholder="Card Number"
-//           onChange={handleInputChange}
-//           onFocus={handleInputFocus}
-//         />
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Card Holder Name"
-//           onChange={handleInputChange}
-//           onFocus={handleInputFocus}
-//         />
-//         <input type="tel" name="cvc" placeholder="CVC" onChange={handleInputChange} onFocus={handleInputFocus} />
-//         <input
-//           type="tel"
-//           name="expiry"
-//           placeholder="Expiration Date"
-//           onChange={handleInputChange}
-//           onFocus={handleInputFocus}
-//         />
-//       </form>
-//     </div>
-//   );
-// }
