506b599618e132c7bc503ef5127b81178a43b36c
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-03 20:40:50 -0300

    feat: ticket to payment auto redirection

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,14 +1,22 @@
 import { Typography } from '@material-ui/core';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import useEnrollment from '../../../hooks/api/useEnrollment';
+import useTicket from '../../../hooks/api/useTicket';
 import NoEnrollmentScreen from './NoEnrollmentScreen';
 import PaymentScreen from './PaymentScreen';
 import TicketScreen from './TicketScreen';
 
 export default function Payment() {
   const [paymentScreen, setPaymentScreen] = useState(false);
+  const { ticket } = useTicket();
   const { enrollment } = useEnrollment();
+
+  useEffect(() => {
+    if (ticket) {
+      setPaymentScreen(true);
+    }
+  }, [ticket]);
 
   if (!enrollment) {
     return (
@@ -22,11 +30,7 @@ export default function Payment() {
   return (
     <>
       <StyledTypography variant="h4">Ingresso e Pagamento</StyledTypography>
-      {paymentScreen ? (
-        <PaymentScreen />
-      ) : (
-        <TicketScreen setPaymentScreen={setPaymentScreen} />
-      )}
+      {paymentScreen ? <PaymentScreen /> : <TicketScreen setPaymentScreen={setPaymentScreen} />}
     </>
   );
 }
