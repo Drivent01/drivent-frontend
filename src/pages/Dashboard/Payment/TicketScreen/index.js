249ca1616599a1da361cad3b4efa73842c30ef4f
Laisses <laisse@gmail.com>
2023-03-04 08:32:37 -0300

    refactor: adapt page to receive success, error or loading options

--- src/pages/Dashboard/Payment/TicketScreen/index.js
+++ src/pages/Dashboard/Payment/TicketScreen/index.js
@@ -1,96 +1,21 @@
-import TicketButtonFinalization from './TicketButtonFinalization';
-import ButtonSelection from '../../../../components/Payment/ButtonSelection';
-import StyledSubtitle from '../../../../components/Subtitle';
-import { useState } from 'react';
-import styled from 'styled-components';
-import HotelModalityButtonSelection from '../../../../components/HotelModalityButtonSelection';
+import { Modalities } from './Modalities';
 import useTicketType from '../../../../hooks/api/useTicketType';
+import StyledSubtitle from '../../../../components/Subtitle';
 
 export default function TicketScreen({ setPaymentScreen }) {
-  const { ticketType } = useTicketType();
-  const [types, setTypes] = useState([]);
-  const [moreOptions, setMoreOptions] = useState(false);
-  const [selectedTicket, setSelectedTicket] = useState({});
+  const { ticketType, ticketTypeLoading, ticketTypeError } = useTicketType();
 
-  if (ticketType) {
-    ticketType.forEach((item) => {
-      if (types.length === 0) {
-        setTypes([item]);
-        return;
-      }
-
-      for (let i = 0; i < types.length; i++) {
-        if (types[i].name === item.name) {
-          if (types[i].price > item.price) {
-            const filterTypes = types.filter((element) => element.name !== item.name);
-            setTypes([...filterTypes, { ...item, includesHotel: true }]);
-          }
-
-          break;
-        }
-
-        if (i === types.length - 1) {
-          setTypes([...types, item]);
-        }
-      }
-    });
+  if (ticketTypeError) {
+    return <StyledSubtitle>Ops... Tivemos um erro </StyledSubtitle>;
   }
 
-  return (
-    <>
-      <StyledSubtitle>Primeiro, escolha sua modalidade de ingresso</StyledSubtitle>
+  if (ticketTypeLoading) {
+    return <StyledSubtitle>Carregando...</StyledSubtitle>;
+  }
 
-      <Container>
-        {ticketType &&
-          types.map((item, index) => (
-            <ButtonSelection
-              key={index}
-              id={item.id}
-              title={item.name}
-              price={item.price}
-              setSelectedTicket={setSelectedTicket}
-              includesHotel={item.includesHotel}
-              setMoreOptions={setMoreOptions}
-            />
-          ))}
-      </Container>
+  if (!ticketType) {
+    return <StyledSubtitle>Os ingressos do envento ainda não estão disponíveis</StyledSubtitle>;
+  }
 
-      {moreOptions && (
-        <>
-          <StyledSubtitle>Ótimo! Agora escolha sua modalidade de hospedagem</StyledSubtitle>
-          <Container>
-            {ticketType.map((item, index) => {
-              if (item.name === selectedTicket.title) {
-                return (
-                  <HotelModalityButtonSelection
-                    key={index}
-                    id={item.id}
-                    title={item.name}
-                    name={item.includesHotel ? 'Com Hotel' : 'Sem Hotel'}
-                    price={selectedTicket.price}
-                    setSelectedTicket={setSelectedTicket}
-                    totalPrice={item.price}
-                  />
-                );
-              }
-            })}
-          </Container>
-        </>
-      )}
-
-      {selectedTicket.concluded && (
-        <StyledSubtitle>Fechado! O total ficou em R$ {selectedTicket.totalPrice}. Agora é só confirmar</StyledSubtitle>
-      )}
-
-      <TicketButtonFinalization reserveData={selectedTicket}>
-        <p className="title"> RESERVAR INGRESSO</p>
-      </TicketButtonFinalization>
-    </>
-  );
+  return <Modalities types={ticketType} />;
 }
-
-const Container = styled.div`
-  display: flex;
-  gap: 24px;
-  margin-bottom: 44px;
-`;
