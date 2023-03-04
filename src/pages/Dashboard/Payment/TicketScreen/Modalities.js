22337d7b5fcc5fcddb85ab79bcf746d286007223
Laisses <laisse@gmail.com>
2023-03-04 11:44:07 -0300

    Refactor: moves the submit ticket logic from the button to the Modality component

--- src/pages/Dashboard/Payment/TicketScreen/Modalities.js
+++ src/pages/Dashboard/Payment/TicketScreen/Modalities.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
+import useSaveTicket from '../../../../hooks/api/useSaveTicket';
 import { TicketModality } from './TicketModality';
 import { HotelModality } from './HotelModality';
 import ButtonFinalization from '../../../../components/Payment/ButtonFinalization';
@@ -9,6 +11,7 @@ export const Modalities = ({ types }) => {
   const [showHotels, setShowHotels] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [ticketModality, setTicketModality] = useState(null);
+  const { saveTicketLoading, saveTicketError, saveTicket } = useSaveTicket();
 
   const typesWithoutHotel = types
     .filter(t => !t.includesHotel)
@@ -17,6 +20,26 @@ export const Modalities = ({ types }) => {
   const typesWithHotel = types
     .filter(t => t.name === 'Presencial')
     .sort((a, b) => a.price - b.price);
+
+  const submitModality = async e => {
+    e.preventDefault();
+
+    if (saveTicketLoading) {
+      toast('Processando seu pedido...');
+    }
+
+    if (saveTicketError) {
+      toast('Houve um erro ao processar sua escolha!');
+      return;
+    }
+
+    try {
+      await saveTicket({ ticketTypeId: ticketModality.id });
+    } catch (err) {
+      toast('Não foi possível salvar sua escolha!');
+      return;
+    }
+  };
 
   return (
     <>
@@ -37,7 +60,7 @@ export const Modalities = ({ types }) => {
           <StyledSubtitle>
             Fechado! O total ficou em <PriceHighlight>R$ {ticketModality.price}</PriceHighlight>. Agora é só confirmar:
           </StyledSubtitle>
-          <ButtonFinalization>
+          <ButtonFinalization onClick={e => submitModality(e)}>
             <p className="title"> RESERVAR INGRESSO</p>
           </ButtonFinalization>
         </>
