90f597e9b3081329e1ce0fa20c09be5f23cc6432
Laisses <laisse@gmail.com>
2023-03-04 10:36:16 -0300

    refactor: create a Modality component to handle wich modality to show and to send to the API

--- src/pages/Dashboard/Payment/TicketScreen/Modalities.js
+++ src/pages/Dashboard/Payment/TicketScreen/Modalities.js
@@ -1,26 +1,51 @@
+import styled from 'styled-components';
 import { useState } from 'react';
 import { TicketModality } from './TicketModality';
 import { HotelModality } from './HotelModality';
 import ButtonFinalization from '../../../../components/Payment/ButtonFinalization';
+import StyledSubtitle from '../../../../components/Subtitle';
 
 export const Modalities = ({ types }) => {
   const [showHotels, setShowHotels] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
-  //const [choosenModality, setChoosenModality] = useState(null);
+  const [ticketModality, setTicketModality] = useState(null);
 
   const typesWithoutHotel = types
     .filter(t => !t.includesHotel)
     .sort((a, b) => b.price - a.price);
+
+  const typesWithHotel = types
+    .filter(t => t.name === 'Presencial')
+    .sort((a, b) => a.price - b.price);
 
   return (
     <>
       <TicketModality
         typesWithoutHotel={typesWithoutHotel}
         setShowHotels={setShowHotels}
-        setShowConfirmation={setShowConfirmation} />
+        setShowConfirmation={setShowConfirmation}
+        setTicketModality={setTicketModality}
+      />
       {showHotels &&
-        <HotelModality />}
-      {showConfirmation && <ButtonFinalization />}
+        <HotelModality
+          typesWithHotel={typesWithHotel}
+          setShowConfirmation={setShowConfirmation}
+          setTicketModality={setTicketModality}
+        />}
+      {showConfirmation &&
+        <>
+          <StyledSubtitle>
+            Fechado! O total ficou em <PriceHighlight>R$ {ticketModality.price}</PriceHighlight>. Agora é só confirmar:
+          </StyledSubtitle>
+          <ButtonFinalization>
+            <p className="title"> RESERVAR INGRESSO</p>
+          </ButtonFinalization>
+        </>
+      }
     </>
   );
 };
+
+const PriceHighlight = styled.span`
+  font-weight: bold;
+`;
