c97b75e6351ef40141c554a3dc0d76f4f5864dd6
erickssguerra <erickssguerra@gmail.com>
2023-03-04 15:08:37 -0300

    feat: add ConfirmationSection

--- src/components/Payment/CreditCard/ConfirmationSection.js
+++ src/components/Payment/CreditCard/ConfirmationSection.js
@@ -1,3 +1,35 @@
+import styled from 'styled-components';
+import { FaCheckCircle } from 'react-icons/fa';
+
 export default function ConfirmationSection(props) {
-  return <></>;
+  return (
+    <ConfirmationContainer>
+      <FaCheckCircle />
+      <MessageContainer>
+        <h3>Pagamento confirmado!</h3>
+        <p>Prossiga para escolha de hospedagem e atividades</p>
+      </MessageContainer>
+    </ConfirmationContainer>
+  );
 }
+
+const ConfirmationContainer = styled.div`
+  display: flex;
+  svg {
+    color: #36b853;
+    font-size: 40px;
+  }
+`;
+
+const MessageContainer = styled.div`
+  display: block;
+  margin-left: 10px;
+  line-height: 20px;
+  color: #454545;
+  h3 {
+    font-weight: 700;
+  }
+  p {
+    font-weight: 300;
+  }
+`;
