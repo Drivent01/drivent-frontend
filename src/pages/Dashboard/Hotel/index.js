066c372c3d8fc8ae07743f7c2eed37ca40e01fd2
Cintra <gui7lk@gmail.com>
2023-03-09 14:00:52 -0300

    fix: hotel screen validate payment

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -9,7 +9,7 @@ export default function Hotel() {
     <>
       {(ticket?.status === 'RESERVED') && (
         <AlertInfoScreen>
-          Sua modalidade de ingresso não inclui hospedagem Prossiga para a escolha de atividades
+          Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem
         </AlertInfoScreen>
       )}
 
