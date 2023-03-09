d070649a672c0ce27ddab3649174722e82464342
erickssguerra <erickssguerra@gmail.com>
2023-03-09 19:52:01 -0300

    feat: create useSaveBooking hook

---
+++ src/hooks/api/useSaveBooking.js
@@ -0,0 +1,20 @@
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+import * as bookingApi from '../../services/bookingApi';
+
+export default function useBooking() {
+  const token = useToken();
+
+  const {
+    loading: saveBookingLoading,
+    error: saveBookingError,
+    act: saveBooking,
+  } = useAsync((body) => bookingApi.postBooking(body, token));
+
+  return {
+    saveBookingLoading,
+    saveBookingError,
+    saveBooking,
+  };
+}
---
+++ src/services/bookingApi.js
@@ -0,0 +1,31 @@
+import api from './api';
+
+export async function postBooking(body, token) {
+  const response = await api.post('/booking', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getTicketType(token) {
+  const response = await api.get('/booking', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getTicket(token) {
+  const response = await api.get('/booking', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
