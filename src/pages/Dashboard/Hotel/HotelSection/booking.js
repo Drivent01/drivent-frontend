d9ecee8da877eff18210a53464dc6cf6fffc08c4
Laisses <laisse@gmail.com>
2023-03-09 10:57:19 -0300

    feat: add chore states and title component to the Booking component

--- src/pages/Dashboard/Hotel/HotelSection/booking.js
+++ src/pages/Dashboard/Hotel/HotelSection/booking.js
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Title } from '../styles';
 import RoomSection from './roomSection';
+import HotelSection from './hotelsSection';
+import ButtonFinalization from '../../../../components/Payment/ButtonFinalization';
 
 export default function Booking({ hotelList }) {
-  const [selectedHotel, setSelectedHotel] = useState({});
+  const [selectedHotel, setSelectedHotel] = useState(null);
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [bookingId, setBookingId] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (bookingId) {
+      navigate('hotel/review');
+    }
+  }, [bookingId]);
+
   return (
     <>
-      {/* {hotelList.map((hotel, index) => {})} */}
-      {selectedHotel && <RoomSection hotel={setSelectedHotel} />}
+      <Title>Escolha de hotel e quarto</Title>
+      {/* {selectedHotel && <RoomSection hotel={setSelectedHotel} />} */}
     </>
   );
 }
