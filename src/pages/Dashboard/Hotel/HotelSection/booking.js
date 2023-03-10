5dedeae12ce581aa55bd7bef393bb2c3da57d9d8
erickssguerra <erickssguerra@gmail.com>
2023-03-09 21:38:35 -0300

    incomplete: booking

--- src/pages/Dashboard/Hotel/HotelSection/booking.js
+++ src/pages/Dashboard/Hotel/HotelSection/booking.js
@@ -1,49 +1,64 @@
-import { /* useEffect, */ useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { Title } from '../styles';
 import RoomSection from './roomSection';
 import HotelSection from './hotelsSection';
 import ButtonFinalization from '../../../../components/Payment/ButtonFinalization';
+import useSaveBooking from '../../../../hooks/api/useSaveBooking';
+import useChangeBooking from '../../../../hooks/api/useChangeBooking';
+import useBooking from '../../../../hooks/api/useBooking';
 
 export default function Booking({ hotelList }) {
   const [selectedHotel, setSelectedHotel] = useState(null);
-  const [roomId, setRoomId] = useState(null);
-  const [/* bookingId */, setBookingId] = useState(null);
+  const { saveBooking } = useSaveBooking();
+  // const { changeBooking } = useChangeBooking();
+  const { booking, bookingLoading } = useBooking();
+  const [roomId, setRoomId] = useState(1);
+  const [bookingId, setBookingId] = useState(null);
   //const [changeBooking, setChangeBooking] = useState(null);
   const navigate = useNavigate();
 
-  /* useEffect(() => {
-    if (bookingId) {
-      navigate('hotel/review');
+  // eslint-disable-next-line
+  useEffect(() => {
+    try {
+      const activeBooking = !bookingLoading && booking;
+      if (activeBooking) {
+        setBookingId(activeBooking);
+      }
+    } catch (err) {
+      console.log(err);
     }
-  }, [bookingId]); */
+  }, [booking]);
 
-  const submitBooking = async e => {
+  /*  console.log(bookingId);
+  console.log(booking); */
+  async function submitBooking(e) {
     e.preventDefault();
 
     try {
-      const booking = await 'mandar o roomID e resposta da API que é um ID';
-      setBookingId(booking);
-      navigate('hotel/review');
+      if (bookingId) {
+      } else if (!bookingId) {
+      }
+      const bookingId = await saveBooking(roomId);
+      // setBookingId(bookingId);
+      navigate('review');
     } catch (error) {
       toast('Houve um erro ao processar a sua reversa');
       return;
     }
-  };
+  }
 
   return (
     <>
       <Title>Escolha de hotel e quarto</Title>
-      <HotelSection hotelList={hotelList} setSelectedHotel={setSelectedHotel} parentSelected={selectedHotel}/>
-      {selectedHotel &&
-        <RoomSection rooms={selectedHotel.Rooms} setRoomId={setRoomId} />
-      }
-      {roomId &&
-        <ButtonFinalization onClick={e => submitBooking(e)}>
+      <HotelSection hotelList={hotelList} setSelectedHotel={setSelectedHotel} parentSelected={selectedHotel} />
+      {selectedHotel && <RoomSection rooms={selectedHotel.Rooms} setRoomId={setRoomId} />}
+      {roomId && (
+        <ButtonFinalization onClick={(e) => submitBooking(e)}>
           <p className="title">RESERVAR QUARTO</p>
         </ButtonFinalization>
-      }
+      )}
     </>
   );
 }
