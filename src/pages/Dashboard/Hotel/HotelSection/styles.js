02b2d00c1e844e53aae6053002a1cc32134bae21
Laisses <laisse@gmail.com>
2023-03-10 16:31:08 -0300

    feat: room section

---
+++ src/pages/Dashboard/Hotel/HotelSection/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 20px;
+`;
+
+export const RoomsContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 35px 0;
+`;
