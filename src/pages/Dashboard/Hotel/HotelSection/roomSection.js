c3744930e24eec67df2cda5ab171c993cf868ab6
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-10 23:12:06 -0300

    feat: hotel and room auto-selection from loading page

--- src/pages/Dashboard/Hotel/HotelSection/roomSection.js
+++ src/pages/Dashboard/Hotel/HotelSection/roomSection.js
@@ -1,10 +1,18 @@
 import StyledSubtitle from '../../../../components/Subtitle';
 import { Room } from '../../../../components/Hotel/Rooms';
 import { Container, RoomsContainer } from './styles';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function RoomSection({ rooms, setRoomId }) {
-  const [selections, setSelections] = useState(rooms.map(_ => false));
+export default function RoomSection({ rooms, setRoomId, roomIdFromApi }) {
+  const [selections, setSelections] = useState(rooms.map((_) => false));
+
+  useEffect(() => {
+    let findRoomIndex = null;
+    rooms.forEach((el, ind) => {
+      if (el.id === roomIdFromApi) findRoomIndex = ind;
+    });
+    setSelected(findRoomIndex, roomIdFromApi);
+  }, []);
 
   const setSelected = (index, id) => {
     setSelections(selections.map((_, i) => i === index));
@@ -15,15 +23,16 @@ export default function RoomSection({ rooms, setRoomId }) {
     <Container>
       <StyledSubtitle>Ótima pedida! Agora escolha seu quarto</StyledSubtitle>
       <RoomsContainer>
-        {rooms.map(({ id, name, capacity, Booking }, i) =>
+        {rooms.map(({ id, name, capacity, Booking }, i) => (
           <Room
             key={name}
             name={name}
             capacity={capacity}
-            occupancy={Booking.length}
+            occupancy={id === roomIdFromApi ? Booking.length - 1 : Booking.length}
             selected={selections[i]}
             setSelected={() => setSelected(i, id)}
-          />)}
+          />
+        ))}
       </RoomsContainer>
     </Container>
   );
