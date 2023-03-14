ed2dfa637fb67881b7500dbff9b0195439d27255
Laisses <laisse@gmail.com>
2023-03-14 10:14:08 -0300

    feat: warning screen for activities

---
+++ src/pages/Dashboard/Activities/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 34px;
+  line-height: 40px;
+`;
