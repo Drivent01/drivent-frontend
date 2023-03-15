dbe1e8a8c7e7b8fde81f064b688d6667fcf4ec49
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-14 21:58:56 -0300

    feat: moved the table downwards

--- src/components/Activities/ActivityColumn.js
+++ src/components/Activities/ActivityColumn.js
@@ -6,7 +6,7 @@ export default function ActivityColumn(props) {
 
   return (
     <ActivityStyledColumn>
-      <h1>{place.name}</h1>
+      <h1 className='place'>{place.name}</h1>
       <div className="activity-box-container">
         {place.activities.map((activity) => (
           <ActivityCard activity={activity} />
@@ -17,10 +17,31 @@ export default function ActivityColumn(props) {
 }
 
 const ActivityStyledColumn = styled.div`
+margin-top: 70px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  scrollbar-width: none !important;
+  .place {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 20px;
+    text-align: center;
+    color: #7B7B7B;
+  }
   .activity-box-container {
+    height: 392px;
+    padding: 12px;
     display: flex;
+    scroll-behavior: smooth;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+      display: none;
+    }
     flex-direction: column;
+    border: 1px solid rgba(0, 0, 0, 0.1);
   }
 `;
